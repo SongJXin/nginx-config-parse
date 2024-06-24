75cd9d04c332b35d51903d239f7508511e06275c
songjianxin <sjx@qq.com>
2024-06-24 10:34:57 +0800

    修改proxy_pass添加try_files情况

--- src/parse/parse_detail.go
+++ src/parse/parse_detail.go
@@ -1,6 +1,9 @@
 package parse
 
-import crossplane "github.com/nginxinc/nginx-go-crossplane"
+import (
+	crossplane "github.com/nginxinc/nginx-go-crossplane"
+	"strings"
+)
 
 func ListenParse(args []string) string {
 	if len(args) > 0 {
@@ -25,6 +28,8 @@ func LocationParse(Directive *crossplane.Directive) (string, string, int) {
 			proxyPass, line = ProxyPassParse(locationDirective)
 		} else if locationDirective.Directive == "alias" {
 			proxyPass, line = AliasParse(locationDirective)
+		} else if locationDirective.Directive == "try_files" {
+			proxyPass, line = TryFilesParse(locationDirective)
 		}
 	}
 	return context, proxyPass, line
@@ -40,6 +45,13 @@ func ProxyPassParse(locationDirective *crossplane.Directive) (string, int) {
 func AliasParse(locationDirective *crossplane.Directive) (string, int) {
 	if len(locationDirective.Args) > 0 {
 		return locationDirective.Args[0], locationDirective.Line
+	}
+	return "", 0
+}
+
+func TryFilesParse(locationDirective *crossplane.Directive) (string, int) {
+	if len(locationDirective.Args) > 0 {
+		return strings.Join(locationDirective.Args, " "), locationDirective.Line
 	}
 	return "", 0
 }
